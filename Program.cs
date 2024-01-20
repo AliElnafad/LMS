81964a4ec41b0d49b5665d66e8603653df886c62
AliElnafad <ali_20170328@fci.helwan.edu.eg>
2024-01-20 16:18:22 +0200

    Test1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace LMS
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.ssss
+            // Add services to the container.sssssss
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
